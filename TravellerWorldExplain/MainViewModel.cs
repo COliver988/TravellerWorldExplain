2ea7555046963c26a688a0c9669a6eec7a1f3fe4
Craig Oliver <coliver988@gmail.com>
2023-05-05 17:21:00 -0400

    set tests to target .net 7 so I could install communittoolkit.maui.markdown properly

---
+++ TravellerWorldExplain/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace MauiMarkupSample
+{
+    [INotifyPropertyChanged]
+    public partial class MainViewModel
+    {
+        [ObservableProperty]
+        private string name;
+        partial void OnNameChanging(string value)
+        {
+            Debug.WriteLine($"Name is about to change to {value}");
+        }
+        partial void OnNameChanged(string value)
+        {
+            Debug.WriteLine($"Name has changed to {value}");
+        }
+    }
+}
--- TravellerWorldExplain/MauiProgram.cs
+++ TravellerWorldExplain/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Markup;
 
 namespace TravellerWorldExplain;
 
@@ -10,6 +11,7 @@ public static class MauiProgram
 		builder
 			.UseMauiApp<App>()
 			.UseMauiCommunityToolkit()
+            .UseMauiCommunityToolkitMarkup()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
