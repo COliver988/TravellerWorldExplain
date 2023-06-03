45d2052b861e5fef98734ad7b3ea891a2467c0d2
Craig Oliver <coliver988@gmail.com>
2023-06-03 17:57:19 -0400

    moved the world explanation to a new service. figure I can add additional services (T5WorldService) if we want to use different rule sets.

--- TravellerWorldExplain/MainPage.xaml.cs
+++ TravellerWorldExplain/MainPage.xaml.cs
@@ -1,51 +1,19 @@
 ﻿using System.Text;
 using TravellerWorldExplain.Models;
+using TravellerWorldExplain.Services;
 
 namespace TravellerWorldExplain;
 
 public partial class MainPage : ContentPage
 {
-    public string[] Atmospheres;
-    public string[] Starports;
-    public string[] Governments;
-    public string[] LawLevels;
-    public string[] TechLevels;
-
-    public async Task<string[]> LoadData(string filePath)
-    {
-        // Open the source file
-        using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(filePath);
-        using StreamReader reader = new StreamReader(fileStream);
-
-        // Create a list to store the lines
-        List<string> lines = new List<string>();
-
-        // Read the file line by line and add to the list
-        string line;
-        while ((line = reader.ReadLine()) != null)
-        {
-            lines.Add(line);
-        }
-
-        // Convert the list to an array and return it
-        return lines.ToArray();
-    }
+    public WorldService worldService;
 
     public MainPage()
 	{
 		InitializeComponent();
 		this.BindingContext = new World();
-        _ = LoadDefaultData();
+        worldService = new WorldService();
 	}
-
-    public async Task LoadDefaultData()
-    {
-        Atmospheres = await LoadData("Atmospheres.txt");
-        Governments = await LoadData("Governments.txt");
-        Starports = await LoadData("Starports.txt");
-        LawLevels = await LoadData("LawLevels.txt");
-        TechLevels = await LoadData("TechLevels.txt");
-    }
 
     private void ExplainClicked(object sender, EventArgs e)
     {
@@ -64,44 +32,8 @@ public partial class MainPage : ContentPage
 
     private void ExplainWorld(World world)
     {
-        List<string> explanation = new List<string>();
-        explanation.Add(LoadPortDefinition(world.Starport[0]));
-        int size = int.Parse(world.Size, System.Globalization.NumberStyles.HexNumber);
-        explanation.Add($"Size: {size * 1000} km diameter");
-        int atmo = int.Parse(world.Atmosphere, System.Globalization.NumberStyles.HexNumber);
-        explanation.Add($"Atmosphere: {Atmospheres[atmo].ToString()}");
-        int hydro = int.Parse(world.Hydrographics, System.Globalization.NumberStyles.HexNumber);
-        explanation.Add($"Hydrographics: {hydro * 10}% +/- 5%");
-        int pop = int.Parse(world.Population, System.Globalization.NumberStyles.HexNumber);
-        explanation.Add($"Population: {Math.Pow(10, pop)}");
-        int govt = int.Parse(world.Government, System.Globalization.NumberStyles.HexNumber);
-        explanation.Add($"Government: {Governments[govt].ToString()}");
-        int law = int.Parse(world.LawLevel, System.Globalization.NumberStyles.HexNumber);
-        explanation.Add($"Law Level: {LawLevels[law].ToString()}");
-        int tech = int.Parse(world.TechLevel, System.Globalization.NumberStyles.HexNumber);
-        explanation.Add($"Tech Level: {TechLevels[tech].ToString()}");
-        explanation.Add(LoadTradeCodes(world));
+        List<string> explanation = worldService.explainWorld(world);
         world.Explanation = string.Join("\n", explanation.ToArray());
-    }
-
-    private string LoadTradeCodes(World world)
-    {
-        return "trade codes here";
-    }
-
-    private string LoadPortDefinition(char starport)
-    {
-        foreach (string portDefinition in Starports)
-        {
-            if (portDefinition[0] == starport)
-                return portDefinition;
-        }
-        return "Starport definition not found";
-    }
-
-    private bool ValidateWorld(World world)
-    {
-		return true;
     }
 }
 
---
+++ TravellerWorldExplain/Services/WorldService.cs
@@ -0,0 +1,81 @@
+﻿using TravellerWorldExplain.Models;
+
+namespace TravellerWorldExplain.Services
+{
+    public class WorldService
+    {
+        private string[] _atmospheres;
+        private string[] _starports;
+        private string[] _governments;
+        private string[] _lawLevels;
+        private string[] _techLevels;
+
+        public WorldService() => LoadDefaultData();
+
+        public List<string> explainWorld(World world)
+        {
+            List<string> explanation = new List<string>();
+            explanation.Add(LoadPortDefinition(world.Starport[0]));
+            int size = int.Parse(world.Size, System.Globalization.NumberStyles.HexNumber);
+            explanation.Add($"Size: {size * 1000} km diameter");
+            int atmo = int.Parse(world.Atmosphere, System.Globalization.NumberStyles.HexNumber);
+            explanation.Add($"Atmosphere: {_atmospheres[atmo].ToString()}");
+            int hydro = int.Parse(world.Hydrographics, System.Globalization.NumberStyles.HexNumber);
+            explanation.Add($"Hydrographics: {hydro * 10}% +/- 5%");
+            int pop = int.Parse(world.Population, System.Globalization.NumberStyles.HexNumber);
+            explanation.Add($"Population: {Math.Pow(10, pop)}");
+            int govt = int.Parse(world.Government, System.Globalization.NumberStyles.HexNumber);
+            explanation.Add($"Government: {_governments[govt].ToString()}");
+            int law = int.Parse(world.LawLevel, System.Globalization.NumberStyles.HexNumber);
+            explanation.Add($"Law Level: {_lawLevels[law].ToString()}");
+            int tech = int.Parse(world.TechLevel, System.Globalization.NumberStyles.HexNumber);
+            explanation.Add($"Tech Level: {_techLevels[tech].ToString()}");
+            explanation.Add(LoadTradeCodes(world));
+            return explanation;
+        }
+
+        private string LoadPortDefinition(char starport)
+        {
+            foreach (string portDefinition in _starports)
+            {
+                if (portDefinition[0] == starport)
+                    return portDefinition;
+            }
+            return "Starport definition not found";
+        }
+
+        private string LoadTradeCodes(World world)
+        {
+            return "trade codes here";
+        }
+        private async Task<string[]> LoadData(string filePath)
+        {
+            // Open the source file
+            using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(filePath);
+            using StreamReader reader = new StreamReader(fileStream);
+
+            // Create a list to store the lines
+            List<string> lines = new List<string>();
+
+            // Read the file line by line and add to the list
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            // Convert the list to an array and return it
+            return lines.ToArray();
+        }
+
+        private async Task LoadDefaultData()
+        {
+            _atmospheres = await LoadData("Atmospheres.txt");
+            _governments = await LoadData("Governments.txt");
+            _starports = await LoadData("Starports.txt");
+            _lawLevels = await LoadData("LawLevels.txt");
+            _techLevels = await LoadData("TechLevels.txt");
+        }
+    }
+
+}
