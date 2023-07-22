3f6d6cfd6105dbd9f2d29dfe8ceb21d319b84a1b
Craig Oliver <coliver988@gmail.com>
2023-07-22 19:13:18 -0400

    corrected freight calculations: lots vs basic tonnage

--- TravellerWorldExplain/Services/FreightService.cs
+++ TravellerWorldExplain/Services/FreightService.cs
@@ -5,7 +5,7 @@ namespace TravellerWorldExplainer.Services
     public class FreightService : TravellerServiceBase
     {
         private string[] _freightRolls;
-        private string[] freightTypes = new string[] { "Major", "Minor", "Incedental" };
+        private string[] freightTypes = new string[] { "Major", "Minor", "Incidental" };
         private int[] multiplier = new int[] { 10, 5, 1 };
 
         public FreightService() => LoadDefaultData();
@@ -15,30 +15,34 @@ namespace TravellerWorldExplainer.Services
         /// </summary>
         /// <param name="world"></param>
         /// <param name="destinationPopulation"></param>
+        /// <remarks>the freight table indicates the number of lots per size to use</remarks>
         /// <returns>list of available freights</returns>
         public List<string> LoadFreight(World world, int destinationPopulation)
         {
             List<string> freight = new List<string>();
             int pop = int.Parse(world.Population, System.Globalization.NumberStyles.HexNumber);
             string[] rolls = _freightRolls[pop].Split(new char[] { ',' });
-            int freightTons = 0;
+            int freightLots;
             for (int i = 0; i <= 2; i++)
             {
-                freightTons = calculateRoll(rolls[i]);
+                freightLots = calculateRoll(rolls[i]);
                 switch (destinationPopulation)
                 {
                     case <= 4:
-                        freightTons -= 3;
+                        freightLots -= 3;
                         break;
                     case >= 8:
-                        freightTons += 3;
+                        freightLots += 3;
                         break;
                    default:
                         break;
                 }
-                if (freightTons < 0)
-                    freightTons = 0;
-                freight.Add($"{freightTypes[i]}: {freightTons * multiplier[i]} dTons");
+                if (freightLots < 0)
+                    freightLots = 0;
+                for (int lots = 0; lots <= freightLots; lots++)
+                {
+                   freight.Add($"{freightTypes[i]}: {calculateRoll("1D") * multiplier[i]} dTons");
+                }
             }
 
             return freight;
